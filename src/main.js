a04b5f27f3b34fa8dd8ef747f991f9159ff836a5
James <james.pearson1996@gmail.com>
2020-10-14 21:03:53 +0100

    Master - Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
+import axios from 'axios'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://gomediaspa.test/'
+
+store.dispatch('auth/me').then(() => {
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount('#app')
+})
 
 new Vue({
   router,
